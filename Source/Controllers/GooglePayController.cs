0aeb3c78c864555daa00e86fb0b9aa52625784ce
Lyndon Chivers <lyndychivs@gmail.com>
2023-11-21 20:47:00 +0000

    update GooglePayDecryptionController

--- Source/Controllers/GooglePayController.cs
+++ Source/Controllers/GooglePayController.cs
@@ -8,16 +8,23 @@
 
     public class GooglePayController : Controller
     {
+        [Route("")]
+        [Route("GooglePay/Sale")]
+        [Route("GooglePay/Sale/{id?}")]
         public IActionResult Sale()
         {
             return View();
         }
 
+        [Route("GooglePay/Authorization")]
+        [Route("GooglePay/Authorization/{id?}")]
         public IActionResult Authorization()
         {
             return View();
         }
 
+        [Route("{id?}")]
+        [Route("{id?}/{value?}")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Source/Controllers/GooglePayDecryptionController.cs
+++ Source/Controllers/GooglePayDecryptionController.cs
@@ -5,25 +5,24 @@
     using GooglePay.PaymentDataCryptography;
 
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Logging;
 
     using Wallet.Models;
 
     [ApiController]
-    [Route("googlepay")]
-    public class GooglePayApiController : ControllerBase
+    public class GooglePayDecryptionController : ControllerBase
     {
         private readonly GoogleKeyProvider _googleKeyProvider;
 
         private readonly PaymentMethodTokenRecipient _paymentMethodTokenRecipient;
 
-        public GooglePayApiController()
+        public GooglePayDecryptionController()
         {
             _googleKeyProvider = new GoogleKeyProvider(true);
             _paymentMethodTokenRecipient = new PaymentMethodTokenRecipient("merchant:12345678901234567890", _googleKeyProvider);
         }
 
         [HttpPost]
+        [Route("GooglePay/Decrypt")]
         public IActionResult Post([FromBody] GooglePayDecryptionRequest googlePayDecryptionRequest)
         {
             return TryDecrypt(googlePayDecryptionRequest);
@@ -34,12 +33,20 @@
             try
             {
                 _paymentMethodTokenRecipient.AddPrivateKey(googlePayDecryptionRequest.PrivateKey);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"{nameof(GooglePayDecryptionRequest)}.{nameof(googlePayDecryptionRequest.PrivateKey)}\n{ex.Message}");
+            }
+
+            try
+            {
                 string decryptedData = _paymentMethodTokenRecipient.Unseal(googlePayDecryptionRequest.EncryptedToken);
                 return Ok(new GooglePayDecryptionResponse() { DecryptedData = decryptedData });
             }
             catch (Exception ex)
             {
-                return BadRequest(ex);
+                return BadRequest($"{nameof(GooglePayDecryptionRequest)}.{nameof(googlePayDecryptionRequest.EncryptedToken)}\n{ex.Message}");
             }
         }
     }
